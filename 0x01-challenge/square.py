c89b2aaa6184f8f2c9fbc5ed3364de72ece97ce4
AlexisCORAN <alexis_ca_@outlook.com>
2021-10-04 05:49:07 +0000

    Updated tasks

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
-""" Module """
+""" 
+This file contains the Square() Class
+"""
 
 
 class Square():
-    """ square class """
+    """
+    square class definition
+    """
+
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Instance"""
+        """ method for Square Instances"""
         if kwargs:
             for key, value in kwargs.items():
                 if (key == 'width' or key == 'height'):
                     setattr(self, key, value)
 
     def area_of_my_square(self):
-        """Area"""
+        """Area of the square"""
         return self.width * self.height
 
     def permiter_of_my_square(self):
-        """Perimeter"""
+        """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
