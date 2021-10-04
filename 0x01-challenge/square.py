7a508f10e360724908054073526391eea0ecedbf
AlexisCORAN <alexis_ca_@outlook.com>
2021-10-04 05:30:14 +0000

    Added tasks

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
+""" Module """
 
-class square():
-    
+
+class Square():
+    """ square class """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Instance"""
         if kwargs:
             for key, value in kwargs.items():
-                setattr(self, key, value)
+                if (key == 'width' or key == 'height'):
+                    setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
